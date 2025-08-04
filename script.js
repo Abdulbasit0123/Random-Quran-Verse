58c76078562e9db9bcabdcd6382954942303cf25
Abdulbasit <82594778+Abdulbasit0123@users.noreply.github.com>
2025-08-04 23:07:01 +0300

    Update script.js

--- script.js
+++ script.js
@@ -109,8 +109,8 @@ async function fetchQuranAndTafseer() {
     try {
         landing.style.display = 'flex';
         const [quranRes, tafseerRes] = await Promise.all([
-            fetch('http://api.alquran.cloud/v1/quran/ar.sahih'),
-            fetch('http://api.alquran.cloud/v1/quran/ku.asan')
+            fetch('https://api.alquran.cloud/v1/quran/ar.sahih'),
+            fetch('https://api.alquran.cloud/v1/quran/ku.asan')
         ]);
 
         if (!quranRes.ok || !tafseerRes.ok) {
@@ -466,3 +466,4 @@ async function init() {
 }
 
 init();
+
